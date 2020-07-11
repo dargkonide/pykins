7d38784f4f4b75a5d27fdf8bbaacc934c2b0e832
dargkonide <58darklord@gmail.com>
2020-07-11 14:32:18 +0300

    *fix dab delete component

--- angular/src/app/pages/job/job.module.ts
+++ angular/src/app/pages/job/job.module.ts
@@ -7,13 +7,11 @@ import { HistoryComponent } from './components/history/history.component';
 import { BuildComponent } from './components/build/build.component';
 import { JobComponent } from './components/job/job.component';
 import { MaterialModule } from 'src/app/util/material/material.module';
-import { TaskComponent } from './components/job/components/task/task.component';
-import { VariablesComponent } from './components/job/components/variables/variables.component';
 import { FormsModule } from '@angular/forms';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
 
 @NgModule({
-  declarations: [JobPageComponent, JobComponent, HistoryComponent, BuildComponent, TaskComponent, VariablesComponent],
+  declarations: [JobPageComponent, JobComponent, HistoryComponent, BuildComponent],
   imports: [
     CommonModule,
     JobRoutingModule,
